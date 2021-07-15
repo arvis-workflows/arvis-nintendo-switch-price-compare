d9635740d1b6a03a99f85dfe26139f3aca5c8fbc
jopemachine <jopemachine@naver.com>
2021-07-15 22:59:55 +0900

    upd

--- test.test.js
+++
@@ -1,9 +0,0 @@
-const arvishTest = require('arvish-test');
-
-test('Basic', async () => {
-	const arvish = arvishTest();
-
-	const result = await arvish('node list.js mario');
-
-	console.log(result);
-});
